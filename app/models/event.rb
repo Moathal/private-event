17931b7201258b7ad668132e6859ee9199536241
Moathal <moathal.s.kachi@gmail.com>
2023-06-02 21:07:07 +0300

    Add pg gem, migrate & add validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  has_many :attendances
+  has_many :users, through: :attendances
+  validates :location presense: true
+  validates :date presense: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :events, through: :attendances
+    has_many :attendances
+    has_many :created_events, class_name: 'Event', foreign_key: 'user_id'
+
+  validates :fullname, presence: true
+  validates :fullname, presence: true
 end
