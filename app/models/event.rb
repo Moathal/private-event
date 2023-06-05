da80499de3e989c5b8256115218f0b13976bd4cc
Moathal <moathal.s.kachi@gmail.com>
2023-06-05 20:24:19 +0300

    Finish users views

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :attendances
   has_many :users, through: :attendances
-  validates :location presense: true
-  validates :date presense: true
+
+  validates :location, presence: true
+  validates :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
     has_many :created_events, class_name: 'Event', foreign_key: 'user_id'
 
   validates :fullname, presence: true
-  validates :fullname, presence: true
+  validates :birthday, presence: true
 end
