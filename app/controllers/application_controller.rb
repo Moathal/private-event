7c071fa52ee0a858ddfb44ef8b9168e7e89510ba
Moathal <moathal.s.kachi@gmail.com>
2023-06-05 13:54:59 +0300

    add other user parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :birthday])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :birthday])
+  end
 end
