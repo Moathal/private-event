eba68ab7c6401b3b59702a373d27bacf5b447e00
Moathal <moathal.s.kachi@gmail.com>
2023-06-06 20:14:26 +0300

    Add new and create actions in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,21 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Event was created succefully'
+    else
+      render :new
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:location, :date)
+  end
 end
