cfbe38ebb9d7b0d91ec0e7f9d2e45df247ea4cea
Moathal <moathal.s.kachi@gmail.com>
2023-06-07 12:32:07 +0300

    Edit associatciation for user and event models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :events, through: :attendances
+    has_many :attended_events, through: :attendances, source: :event
     has_many :attendances
     has_many :events, class_name: 'Event', foreign_key: 'creator_id', dependent: :destroy
 
