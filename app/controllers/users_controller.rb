9bff956ce5de4897e362d859c1c28469282f3774
Moathal <moathal.s.kachi@gmail.com>
2023-06-12 12:34:13 +0300

    Use scope insteadof a variable in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @past_attending_events = @user.attended_events.event.where("date < ?", Date.today)
-    @future_attending_events = @user.attended_events.event.where("date >= ?", Date.today)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,9 @@ class Event < ApplicationRecord
   has_many :attendances, dependent: :destroy
   has_many :attendees, through: :attendances, source: :user
 
+  scope :past_attending_events , -> { where("date < ?", Date.today) }
+  scope :future_attending_events, -> { where("date >= ?", Date.today) }
+
   validates :location, presence: true
   validates :date, presence: true
 end
