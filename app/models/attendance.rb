68d787cf14b4b981dc335985a00f92d5fc86bc39
Moathal <moathal.s.kachi@gmail.com>
2023-06-13 20:20:02 +0300

    Add invite ft to model and active record

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,6 @@
 class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  belongs_to :invited_user, class_name: "User", optional: true
+  enum status: { invite_pending: 0, rejected: -1, accepted: 1 }
 end
--- db/migrate/20230602162644_create_attendances.rb
+++ db/migrate/20230602162644_create_attendances.rb
@@ -4,6 +4,8 @@ class CreateAttendances < ActiveRecord::Migration[7.0]
       t.string :location
       t.references :user, null: false, foreign_key: true
       t.references :event, null: false, foreign_key: true
+      t.references :invited_user, foreign_key: { to_table: :users }
+      t.references :attendances, :status, :integer
 
       t.timestamps
     end
