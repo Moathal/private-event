dfdc169b4c18df7438a2c46b3972b65b81759c4f
Moathal <moathal.s.kachi@gmail.com>
2023-09-28 17:03:44 +0300

    Edit streaming notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,7 @@ class Notification < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
 
-  after_create_commit {broadcast_prepend_to 'notification',  partial: 'layouts/notification', locals: {notification: self, current_user: recipient }}
-  after_update_commit {broadcast_replace_to 'notification',  partial: 'layouts/notification', locals: {notification: self, current_user: recipient }}
-  after_destroy_commit {broadcast_remove_to 'notification',  partial: 'layouts/notification', locals: {notification: self, current_user: recipient }}
+  after_create_commit {broadcast_prepend_to 'notification', target: 'notification', partial: 'layouts/notification', locals: {notification: self, current_user: recipient }}
+  after_update_commit {broadcast_replace_to 'notification', target: 'notification', partial: 'layouts/notification', locals: {notification: self, current_user: recipient }}
+  after_destroy_commit {broadcast_remove_to 'notification', target: 'notification', partial: 'layouts/notification', locals: {notification: self, current_user: recipient }}
 end
