c2d4a036646a7cb66240e2d560695aaf2d57f9d3
Moathal <moathal.s.kachi@gmail.com>
2023-10-03 22:11:55 +0300

    finish turbo stream

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,16 @@
 class EventsController < ApplicationController
   include EventsHelper
   before_action :authenticate_user!, except: [:index]
+  before_action :set_event, only: %i[edit update show destroy invite cancel_invitation attend reject_invite accept_invite]
+  
   def index
     @events = Event.all.where(public: true).or(Event.where(creator: current_user))
   end
 
   def show
-    @event = Event.find_by(id: params[:id])
+    # @event = Event.find_by(id: params[:id])
     if @event.nil?
-      render :index, notice: 'Event is removed by its creator!!'
+      redirect_to events_path, notice: 'Event is removed by its creator!!'
     else
       attending_users = @event.attendees.where('status >= ?', 0) + [@event.creator]
       attending_ids = attending_users.pluck(:id)
@@ -18,20 +20,19 @@ class EventsController < ApplicationController
   end
 
   def new
-    @event = current_user.events.build
+    @event = current_user.events.new
   end
 
   def create
     @event = current_user.events.build(event_params)
     if @event.save
-      render :index, notice: 'Event was created successfully'
+      redirect_to @event, notice: 'Event was created successfully'
     else
       render :new
     end
   end
 
   def attend
-    @event = Event.find_by(id: params[:id])
     user_event = current_user.attended_events.where(id: @event.id)
     attendance = nil
     if user_event.present?
@@ -48,7 +49,7 @@ class EventsController < ApplicationController
       attendance.save
       Attendance.notify_recipient('attend', @event, @event.creator, current_user)
     end
-    redirect_to @event
+    render @event
   end
 
   def invite
@@ -67,7 +68,6 @@ class EventsController < ApplicationController
   end
 
   def cancel_invitation
-    @event = Event.find(params[:id])
     attendance = Attendance.find_by(id: params[:attendance_id])
     if attendance
       attendance.update(status: :canceled, invited_user: false)
@@ -79,11 +79,10 @@ class EventsController < ApplicationController
   end
 
   def accept_invite
-    event = Event.find(params[:id])
-    user_attend = current_user.attendances.find_by(event_id: event.id)
+    user_attend = current_user.attendances.find_by(event_id: @event.id)
     if user_attend.present?
       accept_invite_with_attend_record(user_attend)
-      Attendance.notify_recipient(user_attend.status, event, event.creator, current_user)
+      Attendance.notify_recipient(user_attend.status, @event, @event.creator, current_user)
     else
       flash[:error] = 'We apologize but we didnt find you in the list.'
     end
@@ -91,41 +90,39 @@ class EventsController < ApplicationController
   end
 
   def reject_invite
-    event = Event.find(params[:id])
-    user_attend = current_user.attendances.find_by(event_id: event.id)
+    user_attend = current_user.attendances.find_by(event_id: @event.id)
     if user_attend.present?
       reject_invite_with_attend_record(user_attend)
-      Attendance.notify_recipient(user_attend.status, event, event.creator, current_user)
+      Attendance.notify_recipient(user_attend.status, @event, @event.creator, current_user)
     else
       flash[:error] = 'We apologize but we didnt find you in the list.'
     end
-    redirect_to @event
+    render @event
   end
 
   # Event edit view
   def edit
-    @event = Event.find(params[:id])
   end
 
   # Event edit post
   def update
-    puts '<><><><><><><><><><><><><><><><><><><><><> UPDATE START HERE <><><><><><><><><><><><><><><><><><><><><><><><>'
-    @event = Event.find_by(id: params[:id])
     if @event.update(event_params)
-      redirect_to @event, notice: 'Event updated successfully.'
+      render @event, notice: 'Event updated successfully.'
     else
       render :edit
     end
-    puts '<><><><><><><><><><><><><><><><><><><><><> UPDATE ENDS HERE <><><><><><><><><><><><><><><><><><><><><><><><>'
   end
 
   def destroy
-    @event = Event.find(params[:id])
     @event.destroy
     render :index, notice: 'Event is deleted successfully.'
   end
 
   private
+
+  def set_event
+    @event = Event.find_by(id: params[:id])
+  end
 
   def event_params
     params.require(:event).permit(:name, :location, :date, :public)
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,12 @@ class Notification < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
 
-  after_create_commit {broadcast_prepend_later_to "notification-user_1", partial: 'layouts/notification', locals: {notification: self, current_user: recipient }, target: 'notifications'}
+  after_create_commit do
+    broadcast_prepend_later_to([self.recipient, "notifications"],
+     partial: 'layouts/notification',
+      locals: {notification: self, current_user: recipient },
+      target: 'notifications')
+  end
   # after_update_commit {broadcast_replace_to 'notification', target: 'notification', partial: 'layouts/notification', locals: {notification: self, current_user: recipient }}
   # after_destroy_commit {broadcast_remove_to 'notification', target: 'notification', partial: 'layouts/notification', locals: {notification: self, current_user: recipient }}
 end
